84c1c61edc759f5d3078cfe697455e93be19feee
Alex <alejandro4703@hotmail.es>
2018-12-11 14:04:06 +0100

    Añadido triangulo y modificado algunas restrincciones

--- ManuelAlejandroVillegasGrajales.java
+++ ManuelAlejandroVillegasGrajales.java
@@ -15,6 +15,9 @@ public static void main(String[] args) {
 		case 1:
 			System.out.println("El area del triangulo es: "+ ManuelAlejandroVillegasGrajales.areaTriagunlo());
 			break;
+		case 3:
+			System.out.println("El area del rectangulo es: "+ ManuelAlejandroVillegasGrajales.areaRectangulo());
+			break;
 
 		default:
 			break;
@@ -36,7 +39,7 @@ public static double areaTriagunlo () {
 	do {
 	System.out.println("Intruzca la base: ");
 	base = sc.next();
-		if(base.matches("[1-9]"))
+		if(base.matches("[0-9][0-9]?")&& Double.parseDouble(base) > 0)
 			valido =1;
 		else 
 			System.out.println("el valor es negativo o no es valido");
@@ -44,11 +47,35 @@ public static double areaTriagunlo () {
 	do {
 		System.out.println("Intruzca la altura: ");
 		altura = sc.next();
-			if(base.matches("[1-9]"))
+			if(base.matches("[0-9][0-9]?")&& Double.parseDouble(altura) > 0)
 				valido =1;
 			else 
 				System.out.println("el valor es negativo o no es valido");
 		} while (valido!=1);
 	return (Double.parseDouble(base) * Double.parseDouble(altura)/2.0);
+}
+public static double areaRectangulo () {
+	String base="", altura="";
+	int valido=0;
+	do {
+	System.out.println("Intruzca la base: ");
+	base = sc.next();
+		if(base.matches("[2-9]"))
+			valido =1;
+		else 
+			System.out.println("el valor es negativo o no es valido, recuerda que debe estar comprendido entre 2 y 10");
+	} while (valido!=1);
+	do {
+	System.out.println("Intruzca la base: ");
+	altura = sc.next();
+		if(base.matches("[2-9]"))
+			valido =1;
+		else 
+			System.out.println("el valor es negativo o no es valido, recuerda que debe estar comprendido entre 2 y 10");
+	} while (valido!=1);
+return Double.parseDouble(base) * Double.parseDouble(altura);
+}
+public static double areaTrapecio () {
+	return 0;
 }
 }
